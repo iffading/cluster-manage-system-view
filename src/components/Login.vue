34b4f4ba5b5ae525172f2ccf8fa85d4709505f0c
Sky <805850072@qq.com>
2020-03-04 11:27:17 +0800

    新增插件 vue-cli-plugin-element

---
+++ src/components/Login.vue
@@ -0,0 +1,16 @@
+<template>
+  <el-form ref="form" :model="form" label-width="80px">
+    <el-form-item label="活动名称">
+      <el-input v-model="form.name"></el-input>
+    </el-form-item>
+</template>
+
+<script>
+export default {
+  name: 'Login'
+}
+</script>
+
+<style scoped>
+
+</style>
